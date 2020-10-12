da061984470f59ca7a579ffcb333893de375c26a
Tyemy Kuga <tyemy.kuga@dextra-sw.com>
2020-10-12 17:35:48 -0300

    Extracting dialog edition from edge insets property

--- lib/property_widgets/edge_insets_property.dart
+++ lib/property_widgets/edge_insets_property.dart
@@ -1,8 +1,8 @@
+import 'package:dashbook/property_widgets/widgets/property_4_integer_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import '../story.dart';
 import 'widgets/property_scaffold.dart';
-import 'widgets/property_dialog.dart';
 
 class EdgeInsetsProperty extends StatefulWidget {
   final Property<EdgeInsets> property;
@@ -24,16 +24,56 @@ class _EdgeInsetsPropertyState extends State<EdgeInsetsProperty> {
 
   _EdgeInsetsPropertyState(this._currentEdgeinsets);
 
-  void _valueChange(EdgeInsets value) {
-    _currentEdgeinsets = value;
+  EdgeInsets _parseEdgeInsetValues(bool toAllSides, String uniqueValue, String value1, String value2, String value3, String value4) {
+    try {
+      if (toAllSides) {
+        final double value = double.tryParse(uniqueValue);
+
+        if (value == null) {
+          return null;
+        }
+
+        return EdgeInsets.all(value);
+      } else {
+        final double left = double.tryParse(value1);
+        final double top = double.tryParse(value2);
+        final double right = double.tryParse(value3);
+        final double bottom = double.tryParse(value4);
+
+        if (left == null || top == null || right == null || bottom == null) {
+          return null;
+        }
+
+        return EdgeInsets.fromLTRB(left, top, right, bottom);
+      }
+    } catch (err) {
+      return null;
+    }
+  }
+
+  bool _confirmEdition(bool toAllSides, String uniqueValue, String value1, String value2, String value3, String value4) {
+    EdgeInsets edgetInsetsValue = _parseEdgeInsetValues(toAllSides, uniqueValue, value1, value2, value3, value4);
+
+    if (edgetInsetsValue == null) {
+      return false;
+    } else {
+      _currentEdgeinsets = edgetInsetsValue;
+      return true;
+    }
   }
 
   Future<dynamic> show() => showDialog(
         context: context,
-        child: _DialogForm(
-          _currentEdgeinsets,
-          _valueChange,
-        ),
+        child: FourIntegerForm(
+          _confirmEdition,
+          _currentEdgeinsets.left.toInt(),
+          _currentEdgeinsets.top.toInt(),
+          _currentEdgeinsets.right.toInt(),
+          _currentEdgeinsets.bottom.toInt(),
+          'Left',
+          'Top',
+          'Right',
+          'Bottom')
       );
 
   @override
@@ -66,168 +106,5 @@ class _EdgeInsetsPropertyState extends State<EdgeInsetsProperty> {
         ],
       ),
     );
-  }
-}
-
-class _DialogForm extends StatefulWidget {
-  final Function _valueChange;
-  final EdgeInsets _value;
-
-  _DialogForm(this._value, this._valueChange);
-
-  @override
-  State createState() {
-    return _DialogFormState();
-  }
-}
-
-class _DialogFormState extends State<_DialogForm> {
-  bool _validValues = true;
-  bool _useValueToAllSides = false;
-
-  final _uniqueValueController = TextEditingController();
-
-  final _leftController = TextEditingController();
-  final _topController = TextEditingController();
-  final _rightController = TextEditingController();
-  final _bottomController = TextEditingController();
-
-  @override
-  void initState() {
-    super.initState();
-
-    _uniqueValueController.text = widget._value.left.toInt().toString();
-    if (_allValueEqual()) {
-      _useValueToAllSides = true;
-
-      _leftController.text = _uniqueValueController.text;
-      _topController.text = _uniqueValueController.text;
-      _rightController.text = _uniqueValueController.text;
-      _bottomController.text = _uniqueValueController.text;
-    } else {
-      final value = widget._value;
-
-      _leftController.text = value.left.toInt().toString();
-      _topController.text = value.top.toInt().toString();
-      _rightController.text = value.right.toInt().toString();
-      _bottomController.text = value.bottom.toInt().toString();
-    }
-  }
-
-  bool _allValueEqual() {
-    final value = widget._value;
-
-    List<double> values = [value.left, value.top, value.right, value.bottom];
-    return values.every((v) => v == values[0]);
-  }
-
-  @override
-  Widget build(_) {
-    return PropertyDialog(
-      title: 'Set the edge insets:',
-      content: Column(
-        children: [
-          _validValues ? Container() : Text('Invalid values!'),
-          Row(children: [
-            Text('Same value to all:'),
-            Switch(
-              value: _useValueToAllSides,
-              onChanged: (bool isOn) =>
-                  setState(() => _useValueToAllSides = isOn),
-              activeColor: Colors.blue,
-              inactiveTrackColor: Colors.grey,
-              inactiveThumbColor: Colors.grey,
-            ),
-          ]),
-          _useValueToAllSides
-              ? Container(
-                  child: Container(
-                      width: 100,
-                      child: TextField(controller: _uniqueValueController)),
-                )
-              : Column(
-                  children: [
-                    Row(
-                      children: [
-                        Container(width: 70, child: Text('Left:')),
-                        Container(
-                            width: 100,
-                            child: TextField(controller: _leftController)),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Container(width: 70, child: Text('Top:')),
-                        Container(
-                            width: 100,
-                            child: TextField(controller: _topController)),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Container(width: 70, child: Text('Right:')),
-                        Container(
-                            width: 100,
-                            child: TextField(controller: _rightController)),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Container(width: 70, child: Text('Bottom:')),
-                        Container(
-                            width: 100,
-                            child: TextField(controller: _bottomController)),
-                      ],
-                    ),
-                  ],
-                ),
-        ],
-      ),
-      actions: [
-        FlatButton(
-          child: const Text('Got it'),
-          onPressed: () {
-            final parsedEdgeInsets = _parseEdgeInsetValues();
-
-            if (parsedEdgeInsets == null) {
-              setState(() => _validValues = false);
-            } else {
-              setState(() {
-                _validValues = true;
-                widget._valueChange(parsedEdgeInsets);
-              });
-              Navigator.of(context).pop();
-            }
-          },
-        ),
-      ],
-    );
-  }
-
-  EdgeInsets _parseEdgeInsetValues() {
-    try {
-      if (_useValueToAllSides) {
-        final double value = double.tryParse(_uniqueValueController.text);
-
-        if (value == null) {
-          return null;
-        }
-
-        return EdgeInsets.all(value);
-      } else {
-        final double left = double.tryParse(_leftController.text);
-        final double top = double.tryParse(_topController.text);
-        final double right = double.tryParse(_rightController.text);
-        final double bottom = double.tryParse(_bottomController.text);
-
-        if (left == null || top == null || right == null || bottom == null) {
-          return null;
-        }
-
-        return EdgeInsets.fromLTRB(left, top, right, bottom);
-      }
-    } catch (err) {
-      return null;
-    }
   }
 }
---
+++ lib/property_widgets/widgets/property_4_integer_form.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'property_dialog.dart';
+
+class FourIntegerForm extends StatefulWidget {
+  final Function _confirmEdition;
+  final int _value1;
+  final int _value2;
+  final int _value3;
+  final int _value4;
+
+  final String _label1; 
+  final String _label2; 
+  final String _label3; 
+  final String _label4; 
+
+  FourIntegerForm(this._confirmEdition, this._value1, this._value2, this._value3, this._value4, this._label1, this._label2, this._label3, this._label4);
+
+  @override
+  State createState() {
+    return _FourIntegerFormState();
+  }
+}
+
+class _FourIntegerFormState extends State<FourIntegerForm> {
+  bool _validValues = true;
+  bool _useValueToAll = false;
+
+  final _uniqueValueController = TextEditingController();
+
+  final _firstFieldController = TextEditingController();
+  final _secondFieldController = TextEditingController();
+  final _thirdFieldController = TextEditingController();
+  final _fourthFieldController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _uniqueValueController.text = widget._value1.toString();
+    if (_allValueEqual()) {
+      _useValueToAll = true;
+
+      _firstFieldController.text = _uniqueValueController.text;
+      _secondFieldController.text = _uniqueValueController.text;
+      _thirdFieldController.text = _uniqueValueController.text;
+      _fourthFieldController.text = _uniqueValueController.text;
+    } else {
+      _firstFieldController.text = widget._value1.toString();
+      _secondFieldController.text = widget._value2.toString();
+      _thirdFieldController.text = widget._value3.toString();
+      _fourthFieldController.text = widget._value4.toString();
+    }
+  }
+
+  bool _allValueEqual() {
+    List<int> values = [widget._value1, widget._value2, widget._value3, widget._value4];
+    return values.every((v) => v == values[0]);
+  }
+
+  @override
+  Widget build(_) {
+    return PropertyDialog(
+      title: 'Set values:',
+      content: Column(
+        children: [
+          _validValues ? Container() : Text('Invalid values!'),
+          Row(children: [
+            Text('Same value to all:'),
+            Switch(
+              value: _useValueToAll,
+              onChanged: (bool isOn) =>
+                  setState(() => _useValueToAll = isOn),
+              activeColor: Colors.blue,
+              inactiveTrackColor: Colors.grey,
+              inactiveThumbColor: Colors.grey,
+            ),
+          ]),
+          _useValueToAll
+              ? Container(
+                  child: Container(
+                      width: 100,
+                      child: TextField(controller: _uniqueValueController)),
+                )
+              : Column(
+                  children: [
+                    _FieldWithLabel(label: widget._label1, fieldController: _firstFieldController,),
+                    _FieldWithLabel(label: widget._label2, fieldController: _secondFieldController,),
+                    _FieldWithLabel(label: widget._label3, fieldController: _thirdFieldController,),
+                    _FieldWithLabel(label: widget._label4, fieldController: _fourthFieldController,),
+                  ],
+                ),
+        ],
+      ),
+      actions: [
+        FlatButton(
+          child: const Text('Got it'),
+          onPressed: () {
+
+            bool validValues = widget._confirmEdition(
+              _useValueToAll,
+              _uniqueValueController.text,
+              _firstFieldController.text,
+              _secondFieldController.text, _thirdFieldController.text,
+              _fourthFieldController.text);
+
+            if (validValues) {
+              setState(() {
+                _validValues = true;
+              });
+
+              Navigator.of(context).pop();
+            } else {
+              setState(() => _validValues = false);
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class _FieldWithLabel extends StatelessWidget {
+  final String label;
+  final TextEditingController fieldController;
+
+  _FieldWithLabel({ this.label, this.fieldController });
+
+  @override
+  Widget build (_) {
+    return Row(
+      children: [
+        Container(width: 70, child: Text('$label:')),
+        Container(
+        width: 100,
+        child: TextField(controller: fieldController)),
+      ],
+    );
+  }
+}
