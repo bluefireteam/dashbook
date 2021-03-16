ba402a0f6c01362f4b60318d9a0daed69da3a4ea
Alberto Miola <alberto@superformula.com>
2021-03-16 15:55:18 +0100

    fix: increased stories font size

--- lib/decorator.dart
+++ lib/decorator.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
 abstract class Decorator {
+  const Decorator();
+
   Widget decorate(Widget child);
 }
 
 class CenterDecorator extends Decorator {
+  const CenterDecorator();
+
   @override
   Widget decorate(Widget child) => Center(child: child);
 }
