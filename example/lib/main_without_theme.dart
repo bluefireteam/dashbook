895b0f869aee64400904c29b89e6f7f670adf147
Erick Zanardo <erickzanardoo@gmail.com>
2021-03-29 18:30:44 -0300

    Fixing null theme

--- example/lib/main_without_theme.dart
+++ example/lib/main_without_theme.dart
@@ -1,14 +1,11 @@
 import 'package:example/stories.dart';
-import 'package:flutter/material.dart';
-import 'package:dashbook/dashbook.dart';
 import 'package:example/text_story.dart';
+import 'package:flutter/material.dart';
+
+import 'package:dashbook/dashbook.dart';
 
 void main() {
-  final dashbook = Dashbook.dualTheme(
-      light: ThemeData(),
-      dark: ThemeData.dark(),
-      title: 'Dashbook Example',
-  );
+  final dashbook = Dashbook();
 
   addTextStories(dashbook);
   addStories(dashbook);
