96429d7789a78986e55a521cd9ce1833476361ad
Tyemy Kuga <tyemy.kuga@dextra-sw.com>
2021-10-24 20:18:27 -0300

    pubspec update

--- test/widget/widget_test.dart
+++ test/widget/widget_test.dart
@@ -1,5 +1,6 @@
 import 'package:dashbook/dashbook.dart';
 import 'package:dashbook/src/widgets/keys.dart';
+import 'package:device_frame/device_frame.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -46,6 +47,181 @@ void main() {
       await tester.pumpAndSettle();
 
       expect(find.text('Select a device frame'), findsNothing);
+    });
+
+    testWidgets('can select and apply a device to preview', (tester) async {
+      final selectedDevice = Devices.android.nexus9;
+
+      await tester.pumpDashbook(_getDashbook());
+
+      await tester.tap(find.byKey(kDevicePreviewIcon));
+      await tester.pumpAndSettle();
+
+      final dropDown = find
+          .byWidgetPredicate((widget) => widget is DropdownButton<DeviceInfo>);
+
+      await tester.tap(dropDown);
+      await tester.pumpAndSettle();
+
+      final dropdownItem = find.text(selectedDevice.name).last;
+      await tester.ensureVisible(dropdownItem);
+      await tester.pumpAndSettle();
+
+      await tester.tap(dropdownItem);
+      await tester.pumpAndSettle();
+
+      final selectButton = find.text('Select');
+      await tester.tap(selectButton);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(DeviceFrame), findsOneWidget);
+    });
+
+    testWidgets(
+        'rotate and hide frame are showed when a device to preview is selected',
+        (tester) async {
+      final selectedDevice = Devices.android.nexus9;
+
+      await tester.pumpDashbook(_getDashbook());
+
+      await tester.tap(find.byKey(kDevicePreviewIcon));
+      await tester.pumpAndSettle();
+
+      final dropDown = find
+          .byWidgetPredicate((widget) => widget is DropdownButton<DeviceInfo>);
+
+      await tester.tap(dropDown);
+      await tester.pumpAndSettle();
+
+      final dropdownItem = find.text(selectedDevice.name).last;
+      await tester.ensureVisible(dropdownItem);
+      await tester.pumpAndSettle();
+
+      await tester.tap(dropdownItem);
+      await tester.pumpAndSettle();
+
+      final selectButton = find.text('Select');
+      await tester.tap(selectButton);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(DeviceFrame), findsOneWidget);
+
+      expect(find.byKey(kRotateIcon), findsOneWidget);
+      expect(find.byKey(kHideFrameIcon), findsOneWidget);
+    });
+
+    testWidgets('can clear the device preview selected', (tester) async {
+      final selectedDevice = Devices.android.nexus9;
+
+      await tester.pumpDashbook(_getDashbook());
+
+      await tester.tap(find.byKey(kDevicePreviewIcon));
+      await tester.pumpAndSettle();
+
+      final dropDown = find
+          .byWidgetPredicate((widget) => widget is DropdownButton<DeviceInfo>);
+
+      await tester.tap(dropDown);
+      await tester.pumpAndSettle();
+
+      final dropdownItem = find.text(selectedDevice.name).last;
+      await tester.ensureVisible(dropdownItem);
+      await tester.pumpAndSettle();
+
+      await tester.tap(dropdownItem);
+      await tester.pumpAndSettle();
+
+      final selectButton = find.text('Select');
+      await tester.tap(selectButton);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(DeviceFrame), findsOneWidget);
+
+      await tester.tap(find.byKey(kDevicePreviewIcon));
+      await tester.pumpAndSettle();
+
+      final clearButton = find.text('Clear');
+      await tester.tap(clearButton);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(DeviceFrame), findsNothing);
+      expect(find.byKey(kRotateIcon), findsNothing);
+      expect(find.byKey(kHideFrameIcon), findsNothing);
+    });
+
+    testWidgets('can hide device frame', (tester) async {
+      final selectedDevice = Devices.android.nexus9;
+
+      await tester.pumpDashbook(_getDashbook());
+
+      await tester.tap(find.byKey(kDevicePreviewIcon));
+      await tester.pumpAndSettle();
+
+      final dropDown = find
+          .byWidgetPredicate((widget) => widget is DropdownButton<DeviceInfo>);
+
+      await tester.tap(dropDown);
+      await tester.pumpAndSettle();
+
+      final dropdownItem = find.text(selectedDevice.name).last;
+      await tester.ensureVisible(dropdownItem);
+      await tester.pumpAndSettle();
+
+      await tester.tap(dropdownItem);
+      await tester.pumpAndSettle();
+
+      final selectButton = find.text('Select');
+      await tester.tap(selectButton);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(DeviceFrame), findsOneWidget);
+
+      await tester.tap(find.byKey(kHideFrameIcon));
+      await tester.pumpAndSettle();
+
+      expect(
+        find.byWidgetPredicate(
+            (element) => element is DeviceFrame && !element.isFrameVisible),
+        findsOneWidget,
+      );
+    });
+
+    testWidgets('can rotate device preview', (tester) async {
+      final selectedDevice = Devices.android.nexus9;
+
+      await tester.pumpDashbook(_getDashbook());
+
+      await tester.tap(find.byKey(kDevicePreviewIcon));
+      await tester.pumpAndSettle();
+
+      final dropDown = find
+          .byWidgetPredicate((widget) => widget is DropdownButton<DeviceInfo>);
+
+      await tester.tap(dropDown);
+      await tester.pumpAndSettle();
+
+      final dropdownItem = find.text(selectedDevice.name).last;
+      await tester.ensureVisible(dropdownItem);
+      await tester.pumpAndSettle();
+
+      await tester.tap(dropdownItem);
+      await tester.pumpAndSettle();
+
+      final selectButton = find.text('Select');
+      await tester.tap(selectButton);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(DeviceFrame), findsOneWidget);
+
+      await tester.tap(find.byKey(kRotateIcon));
+      await tester.pumpAndSettle();
+
+      expect(
+        find.byWidgetPredicate((element) =>
+            element is DeviceFrame &&
+            element.orientation == Orientation.landscape),
+        findsOneWidget,
+      );
     });
   });
 }
