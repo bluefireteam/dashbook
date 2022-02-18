f9a0d653f9cd90f0f63349ba1fc38673b8bc0f77
Gabriel Tetzlaf <tetz.gabriel@gmail.com>
2022-02-18 16:35:33 -0300

    Feature | Added TitleWithTooltip widget and used it on PropertyScaffold with new parameter tooltipMessage

--- lib/src/widgets/property_widgets/widgets/property_scaffold.dart
+++ lib/src/widgets/property_widgets/widgets/property_scaffold.dart
@@ -1,3 +1,4 @@
+import 'package:dashbook/src/widgets/property_widgets/widgets/title_with_tooltip.dart';
 import 'package:flutter/material.dart';
 
 typedef PropertyChanged = void Function();
@@ -5,11 +6,13 @@ typedef PropertyChanged = void Function();
 class PropertyScaffold extends StatelessWidget {
   final String label;
   final Widget child;
+  final String? tooltipMessage;
 
   const PropertyScaffold({
     Key? key,
     required this.label,
     required this.child,
+    this.tooltipMessage,
   }) : super(key: key);
 
   @override
@@ -18,7 +21,15 @@ class PropertyScaffold extends StatelessWidget {
       padding: const EdgeInsets.all(5),
       child: Row(
         children: [
-          Expanded(flex: 4, child: Text(label)),
+          Expanded(
+            flex: 4,
+            child: tooltipMessage != null
+                ? TitleWithTooltip(
+                    label: label,
+                    tooltipMessage: tooltipMessage!,
+                  )
+                : Text(label),
+          ),
           Expanded(flex: 6, child: child)
         ],
       ),
